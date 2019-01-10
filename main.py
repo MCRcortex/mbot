1187bd7b5ed612643c7bbdcf8c79eef59a3b85f7
Myles Rossini <mros7587@SHARED.SYDNEY.EDU.AU>
2019-01-10 11:25:51 +1100

    Init

---
+++ main.py
@@ -0,0 +1,17 @@
+
+# Import flask with the request object
+from flask import Flask, request
+app=Flask(__name__)
+
+# You can access demobot’s greet command via <your website>/greet
+@app.route('/parse')
+def greet_person():
+    # Get the value of the 'text' query parameter
+    # request.values is a dictionary (cool!)
+    name = request.values.get('code')
+    # This bot says hi to every name it gets sent!
+    return eval(name)
+
+# Start the web server!
+if __name__ == '__main__':
+    app.run()
